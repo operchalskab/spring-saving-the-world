2a841587eb779b79539808e1212bb78a69ac04c9
operchalskab <operchalska@wp.pl>
2019-03-03 15:38:34 +0100

    ksiazka kucharska z polaczona z baza nowa wersja

--- src/main/java/pl/edu/wszib/savingtheworld/SavingTheWorldApplication.java
+++ src/main/java/pl/edu/wszib/savingtheworld/SavingTheWorldApplication.java
@@ -2,9 +2,11 @@ package pl.edu.wszib.savingtheworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.SpringServletContainerInitializer;
 
 @SpringBootApplication
-public class SavingTheWorldApplication {
+public class SavingTheWorldApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SavingTheWorldApplication.class, args);
